7e3be80b7d4333c1b27f75469fc6572aec716541
ArthurChenEvans <arthurchenevans@protonmail.com>
2024-03-08 17:44:29 +0000

    Added hold and reset game functions

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,8 @@ let totalScore = document.querySelector(".selected-total-score");
 let currentScores = document.querySelectorAll(".card-box h3");
 let totalScores = document.querySelectorAll(".card h1");
 let players = document.querySelectorAll(".card");
+let hold = document.querySelector("#hold");
+let newGameButton = document.querySelector("#new-game");
 
 function rollDice() {
     let dice = randomInt(6);
@@ -27,7 +29,6 @@ function switchPlayer()
     })
 
     currentScores.forEach(score => {
-        console.log(score);
         score.classList.toggle("selected-current-score");
     })
 
@@ -39,5 +40,34 @@ function switchPlayer()
     totalScore = document.querySelector(".selected-total-score");
 }
 
-document.querySelector("#roll-dice").addEventListener('click', () => rollDice());
+function resetGame() {
+    players.forEach(player => {
+        player.querySelector("h1").textContent = "0";
+        player.querySelector(".card-box h3").textContent = "0";
+        player.classList.remove("selected");
+        player.querySelector("h1").classList.remove("selected-total-score");
+        player.querySelector(".card-box h3").classList.remove("selected-current-score");
+    });
 
+    document.querySelector(".card").classList.add("selected");
+    document.querySelector(".card h1").classList.add("selected-total-score");
+    document.querySelector(".card-box h3").classList.add("selected-current-score");
+    currentScore = document.querySelector(".selected-current-score");
+    totalScore = document.querySelector(".selected-total-score");
+}
+
+document.querySelector("#roll-dice").addEventListener('click', () => rollDice());
+hold.addEventListener('click', () => {
+    let player = document.querySelector(".selected h2");
+    if (Number(totalScore.textContent) >= 100)
+    {
+        alert(`${player.textContent} wins!`);
+        resetGame();
+    }
+    else
+    {
+        switchPlayer();
+    }
+});
+
+newGameButton.addEventListener('click', () => resetGame());
